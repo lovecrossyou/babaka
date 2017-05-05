a24391e600116de848856e927bcbb49757aca3d3
JessieJ <85274001@qq.com>
2017-05-05 17:36:14 +0800

    xxx

--- babaka.py
+++ babaka.py
@@ -1,6 +1,21 @@
 from flask import Flask,render_template
+import sqlite3
+from flask import g
 
+DATABASE = '/path/to/database.db'
 app = Flask(__name__)
+
+def connect_db():
+    return sqlite3.connect(DATABASE)
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    if hasattr(g, 'db'):
+        g.db.close()
 
 
 @app.route('/')
