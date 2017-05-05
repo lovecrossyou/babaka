2666ebe97fec215e382ffd7ad6a7e7ec403a0ebb
JessieJ <85274001@qq.com>
2017-05-05 13:50:06 +0800

    xxx

--- babaka.py
+++ babaka.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('error.html'), 404
+
 
 if __name__ == '__main__':
     app.run()
